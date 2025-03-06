1bd24862126366f2a0ef628dbcd43d5fd28096d8
Двниил Архипов <erezer@MacBook-Air-Dvniil.local>
2025-03-06 16:55:15 +0700

    fix: burger

--- public/js/burger-menu.js
+++ public/js/burger-menu.js
@@ -1,4 +1,10 @@
 document.getElementById('burger-menu').addEventListener('click', function() {
     // Переключаем класс 'active' для меню
     document.getElementById('nav-menu').classList.toggle('active');
+
+    // Получаем заголовок
+    const siteTitle = document.getElementById('site-title');
+
+    // Переключаем класс 'burger-open' для заголовка
+    siteTitle.classList.toggle('burger-open');
 });
