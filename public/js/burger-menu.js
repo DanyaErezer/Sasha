780995bf6069d9e285605461979ab4a3542e1250
Двниил Архипов <erezer@MacBook-Air-Dvniil.local>
2025-03-06 17:15:48 +0700

    fix: burger v.2

--- public/js/burger-menu.js
+++ public/js/burger-menu.js
@@ -1,10 +1,19 @@
-document.getElementById('burger-menu').addEventListener('click', function() {
-    // Переключаем класс 'active' для меню
+document.getElementById('burger-menu').addEventListener('click', function(event) {
+    event.stopPropagation(); // Останавливаем всплытие события
     document.getElementById('nav-menu').classList.toggle('active');
 
-    // Получаем заголовок
     const siteTitle = document.getElementById('site-title');
-
-    // Переключаем класс 'burger-open' для заголовка
     siteTitle.classList.toggle('burger-open');
+});
+
+// Закрываем бургер-меню при нажатии вне его
+document.addEventListener('click', function(event) {
+    const navMenu = document.getElementById('nav-menu');
+    const burgerMenu = document.getElementById('burger-menu');
+
+    if (navMenu.classList.contains('active') && !burgerMenu.contains(event.target)) {
+        navMenu.classList.remove('active'); // Закрываем меню
+        const siteTitle = document.getElementById('site-title');
+        siteTitle.classList.remove('burger-open'); // Возвращаем заголовок на место
+    }
 });
