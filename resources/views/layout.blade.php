ff5fb206c75a35d90c01df84e8e27efa9772a6bb
Двниил Архипов <erezer@MacBook-Air-Dvniil.local>
2025-03-06 17:36:54 +0700

    fix: burger js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/styleHeader.css') }}">
-    <link rel="stylesheet" href="{{ asset ('js/burger-menu.js') }}">
+    <script src="js/burger-menu.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -54,12 +54,7 @@
 
 
 @include('footer')
-<script>
-    document.getElementById('burger-menu').addEventListener('click', function() {
-        const navMenu = document.getElementById('nav-menu');
-        navMenu.classList.toggle('active');
-    });
-</script>
+
 
 </body>
 </html>
