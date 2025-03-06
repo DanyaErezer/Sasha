eda0bb12f527e75b46b10cfdd89537fddf009164
Двниил Архипов <erezer@MacBook-Air-Dvniil.local>
2025-03-06 17:41:18 +0700

    fix: burger js v.2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/styleHeader.css') }}">
-    <script src="js/burger-menu.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -55,6 +54,6 @@
 
 @include('footer')
 
-
+<script src="{{ asset ('js/burger-menu.js') }}"></script>
 </body>
 </html>
