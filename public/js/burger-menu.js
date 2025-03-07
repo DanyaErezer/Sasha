2aae932251ae935f04f3abb8b92b4f626ed827c0
Двниил Архипов <erezer@MacBook-Air-Dvniil.local>
2025-03-07 13:57:09 +0700

    fix: burger v.4

--- public/js/burger-menu.js
+++ public/js/burger-menu.js
@@ -13,6 +13,22 @@ document.getElementById('burger-menu').addEventListener('click', function(event)
     siteTitle.classList.toggle('burger-open');
 });
 
-// Закрываем бургер-меню при клике вне меню
+// Добавляем обработчик клика на ссылки внутри меню
+document.querySelectorAll('#nav-menu a').forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        // Останавливаем всплытие события, чтобы клик по ссылке не закрывал меню
+        event.stopPropagation();
+    });
+});
 
+// Добавляем обработчик клика на документ, чтобы закрыть меню при клике вне его
+document.addEventListener('click', function(event) {
+    const navMenu = document.getElementById('nav-menu');
+    const burgerMenu = document.getElementById('burger-menu');
 
+    // Проверяем, был ли клик вне меню и бургера
+    if (!navMenu.contains(event.target) && !burgerMenu.contains(event.target)) {
+        navMenu.classList.remove('active');
+        document.getElementById('site-title').classList.remove('burger-open');
+    }
+});
