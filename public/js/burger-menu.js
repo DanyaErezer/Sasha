c336fbad2cedca5a7b1bc1b6ac8c5e7535d8321b
Двниил Архипов <erezer@MacBook-Air-Dvniil.local>
2025-03-07 13:44:12 +0700

    fix: burger v.3

--- public/js/burger-menu.js
+++ public/js/burger-menu.js
@@ -14,14 +14,5 @@ document.getElementById('burger-menu').addEventListener('click', function(event)
 });
 
 // Закрываем бургер-меню при клике вне меню
-document.addEventListener('click', function(event) {
-    const navMenu = document.getElementById('nav-menu');
-    const burgerMenu = document.getElementById('burger-menu');
 
-    // Проверяем, открыто ли меню и кликнули ли мы вне бургер-меню и самого меню
-    if (navMenu.classList.contains('active') && !burgerMenu.contains(event.target) && !navMenu.contains(event.target)) {
-        navMenu.classList.remove('active'); // Закрываем меню
-        const siteTitle = document.getElementById('site-title');
-        siteTitle.classList.remove('burger-open'); // Возвращаем заголовок на место
-    }
-});
+
